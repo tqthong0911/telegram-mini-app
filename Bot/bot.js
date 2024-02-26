2fcbe68fea35e6951592521adf40604fe8dd4c74
thong.tran <thong.tran@cubicstack.net>
2024-02-26 15:19:43 +0700

    temp26

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "7052192126:AAHPoAjSQUBvMf1CJfEXYokPrX2D9YGbYdQ";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://telegram-mini-app-ten.vercel.app/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
